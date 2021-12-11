676d7f3d126c046c792b0d5bd1854474cefe6f27
Andrew Eames <ajerxs@gmail.com>
2021-12-11 14:09:16 -0500

    commit tree 6cbd36930f65d271d574a393e51283c21911f4d6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// function superbowlWin(s) {
+//     if(s.result === "W") {
+//         return s.year
+//     }
+// }
 
+// array.find(superbowlWin(s) {
+//     return s.year
+// })
+
+function superbowlWin(array) {
+    let team = array.find(function(s) { return s.result === "W"} );
+    if(team) {
+        return team.year;
+    } else {
+        return team
+    }
+}
