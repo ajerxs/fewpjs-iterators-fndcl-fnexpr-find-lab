4adb21de63b58508c17ee90bba82b71cc4a0f855
Andrew Eames <ajerxs@gmail.com>
2021-12-11 14:09:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(array) {
+    let team = array.find(function(s) { return s.result === "W"} );
+    if(team) {
+        return team.year;
+    } else {
+        return team
+    }
+}
